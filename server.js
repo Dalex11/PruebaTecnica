c350595d62efa89d34bf1c5d964ff9e64250193c
Dalex11 <54454160+Dalex11@users.noreply.github.com>
2022-09-15 20:13:41 -0500

    Update login
    Actualización login y ruta protegida

--- server.js
+++ server.js
@@ -1,12 +1,50 @@
 const express = require('express');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const { serializeUser, deserializeUser } = require('passport');
+const passportLocal = require('passport-local').Strategy;
+
 const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser('secreto'));
+app.use(session({
+    secret: 'secreto',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new passportLocal(function(username, password, done){
+    if(username === "Deiby" && password === "Deiby")
+        return done(null, {id: 1, name: "Deiby"});
+
+    done(null, false);
+}));
+
+//serialize
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
+
+//deserialize
+passport.deserializeUser(function(id, done){
+    done(null, {id: 1, name: "Deiby"});
+});
 
 app.set('view engine', 'ejs');
 
-app.get("/",(req, res)=>{
+app.get("/",  (req, res, next) => {
+    if(req.isAuthenticated()) return next();
+
+    res.redirect("/login");
+},(req, res)=>{
     //si ya iniciamos sesión mostrar las noticias
 
     //si no hemos iniciado sesión redireccionar a /login
+    res.send("Hola");
 });
 
 app.get("/login",(req, res)=>{
@@ -14,8 +52,9 @@ app.get("/login",(req, res)=>{
     res.render("login");
 });
 
-app.post("/login",(req, res)=>{
-    //recibir credenciales e iniciar sesión
-});
+app.post("/login", passport.authenticate('local', {
+    successRedirect: "/",
+    failureRedirect: "/login"
+}));
 
 app.listen(8080, ()=> console.log("Server started"));
